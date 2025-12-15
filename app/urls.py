bcef9a5fbd95d31656e296b361c64e92ff594416
Marco Silva <marcosilvam159@gmail.com>
2025-12-14 22:58:18 -0300

    CRUDE for brands,categories,inflows,outlows,suppliers --- WebPage created with Bootstrap

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    
+    path('', include('brands.urls')),
+    path('', include('categories.urls')),
+    path('', include('suppliers.urls')),
+    path('', include('inflows.urls')),
+    path('', include('outflows.urls')),
 ]
--- brands/admin.py
+++ brands/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Brand  
+from . import models
 
 
-@admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ('name','description','created_at','updated_at')
+    list_display = ('name','description')
+    search_fields = ('name',)
+    
+admin.site.register(models.Brand, BrandAdmin)
